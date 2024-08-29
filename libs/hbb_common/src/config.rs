2740f1b656cf0e38e4927b183d66a9329264f13a
toogopc <admin@sooyl.com>
2024-08-29 16:20:57 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["iz1.cn:216"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["iz1.cn"];
 pub const PUBLIC_RS_PUB_KEY: &str = "7+efDtxRqU5dJ8x5XUnDp5fVFrSotF2zUnCsBSWEflI=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
